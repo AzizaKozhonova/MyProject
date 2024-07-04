b92735ab2638cbc44f8c7b8dd558edd4162c7ad8
Aizhamal <aizhamal.batyrbekova@alatoo.edu.kg>
2024-07-05 00:02:23 +0600

    first commit

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import NavabarContexts from "./contexts/NavabarContexts";
+import AuthContextProvider from "./contexts/AuthContextProvider";
+import ProductContextProvider from "./contexts/ProductContextProvider";
+import CartContextProvider from "./contexts/CartContextProvider";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+    <BrowserRouter>
+        <CartContextProvider>
+            <ProductContextProvider>
+                <AuthContextProvider>
+                    <NavabarContexts>
+                        <App />
+                    </NavabarContexts>
+                </AuthContextProvider>
+            </ProductContextProvider>
+        </CartContextProvider>
+    </BrowserRouter>
+);
